66300a33db1b490bd57d65881c0fbc142dd7bcdd
jordanchan <jordancmchan@hotmail.com>
2021-02-18 00:39:09 -0500

    added stuff

---
+++ javascript/blogScript.js
@@ -0,0 +1,24 @@
+// const helpersApp = {};
+
+
+
+
+
+
+
+
+
+// helpersApp.init = function (){
+     
+     
+     
+     let contactForm = document.querySelector('.blogForm');
+     contactForm.addEventListener('submit', function (event){
+          event.preventDefault();
+          console.log('fdasf');
+     })
+
+
+// }
+
+// helpersApp.init(); 
